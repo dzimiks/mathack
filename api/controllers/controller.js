d4876f373956d7e6d2f2903c2ceaa76cae781e62
dzimiks <vana997@gmail.com>
2018-05-12 22:12:39 +0200

    Add new coords

--- api/controllers/controller.js
+++ api/controllers/controller.js
@@ -1,10 +1,21 @@
-var exports = module.exports = {}
+var exports = module.exports = {};
 
 exports.explore = function (req, res) {
-    console.log(req.body.city);
-    res.status(200).json({
-        "path": [{lat: 82.0012, lng: 13.314}, {lat: -52.123, lng: 13.341}]
-    });
+  console.log(req.body.city);
+  res.status(200).json({
+    "path": [
+      {lat: 44.817186, lng: 20.560856},
+      {lat: 44.835186, lng: 20.530856},
+      {lat: 44.834186, lng: 20.460856},
+      {lat: 44.817186, lng: 20.560856},
+      {lat: 44.812186, lng: 20.560856},
+      {lat: 44.814186, lng: 20.560856},
+      {lat: 44.817186, lng: 20.560846},
+      {lat: 44.817186, lng: 20.530856},
+      {lat: 44.811184, lng: 20.560856},
+      {lat: 44.812183, lng: 20.530856},
+    ]
+  });
 };
 
 
