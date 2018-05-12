5ad4e7015b1414b213f904cc4d1acbfd88ca6f8c
dzimiks <vana997@gmail.com>
2018-05-13 01:33:33 +0200

    Fix landing page

--- client/src/app/explore/explore.component.ts
+++ client/src/app/explore/explore.component.ts
@@ -159,59 +159,55 @@ export class ExploreComponent implements OnInit {
         map: map
       });
 
-      let infowindow = new google.maps.InfoWindow({
+      let infowindow1 = new google.maps.InfoWindow({
         content: 'Marker ' + (markerCounter++) + '<br>Latitude: ' + location.lat() + '<br>Longitude: ' + location.lng()
       });
 
       google.maps.event.addListener(marker, 'click', function () {
-        infowindow.open(map, marker);
+        infowindow1.open(map, marker);
       });
     }
 
     const labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
-    // const locations = [
-    //   { lat: -31.563910, lng: 147.154312 },
-    //   { lat: -33.718234, lng: 150.363181 },
-    //   { lat: -33.727111, lng: 150.371124 },
-    //   { lat: -33.848588, lng: 151.209834 },
-    //   { lat: -33.851702, lng: 151.216968 },
-    //   { lat: -34.671264, lng: 150.863657 },
-    //   { lat: -35.304724, lng: 148.662905 },
-    //   { lat: -36.817685, lng: 175.699196 },
-    //   { lat: -36.828611, lng: 175.790222 },
-    //   { lat: -37.750000, lng: 145.116667 },
-    //   { lat: -37.759859, lng: 145.128708 },
-    //   { lat: -37.765015, lng: 145.133858 },
-    //   { lat: -37.770104, lng: 145.143299 },
-    //   { lat: -37.773700, lng: 145.145187 },
-    //   { lat: -37.774785, lng: 145.137978 },
-    //   { lat: -37.819616, lng: 144.968119 },
-    //   { lat: -38.330766, lng: 144.695692 },
-    //   { lat: -39.927193, lng: 175.053218 },
-    //   { lat: -41.330162, lng: 174.865694 },
-    //   { lat: -42.734358, lng: 147.439506 },
-    //   { lat: -42.734358, lng: 147.501315 },
-    //   { lat: -42.735258, lng: 147.438000 },
-    //   { lat: -43.999792, lng: 170.463352 }
-    // ];
-
-    // let markers = locations.map(function(location, i) {
-    //   return new google.maps.Marker({
-    //     position: location,
-    //     label: labels[i % labels.length]
-    //   });
-    // });
-
     const locations = [
 
     ];
 
-    // this.setMarkers(map, locations);
+    // let infoWindow = new google.maps.InfoWindow();
 
-    // let markerCluster = new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
+    // if (navigator.geolocation) {
+    //   navigator.geolocation.getCurrentPosition(function(position) {
+    //     let pos = {
+    //       lat: position.coords.latitude,
+    //       lng: position.coords.longitude
+    //     };
+    //
+    //     infoWindow.setPosition(pos);
+    //     infoWindow.setContent('Location found.');
+    //     infoWindow.open(map);
+    //     map.setCenter(pos);
+    //   }, function() {
+    //     this.handleLocationError(true, infoWindow, map.getCenter());
+    //   });
+    // } else {
+    //   // Browser doesn't support Geolocation
+    //   this.handleLocationError(false, infoWindow, map.getCenter());
+    // }
 
     //  END
+  }
+
+  handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+      'Error: The Geolocation service failed.' :
+      'Error: Your browser doesn\'t support geolocation.');
+    infoWindow.open(map);
+  }
+
+  getInfo() {
+
   }
 
   setMarkers(locations) {
