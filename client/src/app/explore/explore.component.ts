cdd2cea615b1bde03d451ee4ebc2379441be563a
unknown <brankofulurija@gmail.com>
2018-05-12 19:45:01 +0200

    Added slider percentage

--- client/src/app/explore/explore.component.ts
+++ client/src/app/explore/explore.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Input } from '@angular/compiler/src/core';
 
 @Component({
   selector: 'app-explore',
@@ -8,11 +9,21 @@ import { Component, OnInit } from '@angular/core';
 export class ExploreComponent implements OnInit {
   lat: number = 51.678418;
   lng: number = 7.809007;
+
+  sportSlider: any = 50;
+  historicalSlider: any = 50;
+  natureSlider: any = 50;
+  nightlifeSlider: any = 50;
+
   private config = { hour: 7, minute: 15, meriden: 'PM', format: 12 };
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  sliderChange() {
+    console.log(this.sportSlider);
   }
 
 }
