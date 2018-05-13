f74ebc24f7b2bb421352c3368462514af2c1b9bf
dzimiks <vana997@gmail.com>
2018-05-13 03:18:26 +0200

    Add routes to map

--- client/src/app/explore/explore.component.ts
+++ client/src/app/explore/explore.component.ts
@@ -140,13 +140,130 @@ export class ExploreComponent implements OnInit {
       // styles: mapStyle
     };
 
+    let directionsDisplay = new google.maps.DirectionsRenderer;
+    let directionsService = new google.maps.DirectionsService;
     map = new google.maps.Map(document.getElementById('googleMap'), options);
+    directionsDisplay.setMap(map);
+
+    // calculateAndDisplayRoute(directionsService, directionsDisplay);
+    calculateAndDisplayRouteWaypoints(directionsService, directionsDisplay);
+
+    function calculateAndDisplayRouteWaypoints(directionsService, directionsDisplay) {
+      let waypts = [
+        {
+          location: {
+            lat: 44.814839769142665,
+            lng: 20.453407048071995
+          },
+          stopover: true
+        },
+        {
+          location: {
+            lat: 44.81680775934,
+            lng: 20.45063476598
+          },
+          stopover: true
+        },
+        {
+          location: {
+            lat: 44.818816,
+            lng: 20.453566
+          },
+          stopover: true
+        },
+        {
+          location: {
+            lat: 44.81260725934,
+            lng: 20.45063473598
+          },
+          stopover: true
+        }
+      ];
+
+      directionsService.route({
+        origin: {
+          lat: 44.816894,
+          lng: 20.457857
+        },
+        destination: {
+          lat: 44.8239429546728,
+          lng: 20.45710078162467
+        },
+        waypoints: waypts,
+        optimizeWaypoints: true,
+        travelMode: 'WALKING'
+      }, function(response, status) {
+        if (status === 'OK') {
+          directionsDisplay.setDirections(response);
+
+          let route = response.routes[0];
+          let geocoded_waypoint = response.geocoded_waypoints[0];
+          let summaryPanel = document.getElementById('directions-panel');
+          summaryPanel.innerHTML = '';
+
+          // For each route, display summary information.
+          for (let i = 0; i < route.legs.length; i++) {
+            let routeSegment = i + 1;
+
+            summaryPanel.innerHTML += '<b>Route Segment: ' + routeSegment +
+              '</b><br>From: ';
+            summaryPanel.innerHTML += route.legs[i].start_address + '<br>To: ';
+            summaryPanel.innerHTML += route.legs[i].end_address + '<br>Distance: ';
+            summaryPanel.innerHTML += route.legs[i].distance.text + '<br>Duration: ';
+            summaryPanel.innerHTML += route.legs[i].duration.text + '<br>Summary: ';
+            summaryPanel.innerHTML += route.summary + '<br>Place ID: ';
+            summaryPanel.innerHTML += geocoded_waypoint.place_id + '<br><br>';
+          }
+        } else {
+          window.alert('Directions request failed due to ' + status);
+        }
+      });
+    }
+
+    //   <!--Card content-->
+    // <div class="card-body">
+    // <!--Title-->
+    // <h4 class="card-title">Nature</h4>
+    //   <!--Text-->
+    //   <div class="slidecontainer">
+    //   <label>{{ natureSlider }} %</label>
+    // <input type="range" min="0" max="100" value="50" class="slider" [(ngModel)]="natureSlider" (ngModelChange)="sliderChange()">
+    //   </div>
+    //   </div>
+    //
+    //   </div>
+    //   </div>
+
+    function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+      let selectedMode = 'DRIVING';
+
+      directionsService.route({
+        origin: {
+          lat: 44.8170117585242,
+          lng: 20.458796063476598
+        },
+        destination: {
+          lat: 44.82394295466728,
+          lng: 20.457100781162467
+        },
+        // Note that Javascript allows us to access the constant
+        // using square brackets and a string value as its
+        // "property."
+        travelMode: google.maps.TravelMode[selectedMode]
+      }, function (response, status) {
+        if (status == 'OK') {
+          directionsDisplay.setDirections(response);
+        } else {
+          window.alert('Directions request failed due to ' + status);
+        }
+      });
+    }
 
     // Map 1
-    let marker1 = new google.maps.Marker({
-      position: republicSquareCoords,
-      map: map
-    });
+    // let marker1 = new google.maps.Marker({
+    //   position: republicSquareCoords,
+    //   map: map
+    // });
 
     google.maps.event.addListener(map, 'rightclick', function (event) {
       placeMarker(map, event.latLng);
@@ -168,7 +285,38 @@ export class ExploreComponent implements OnInit {
       });
     }
 
-    const labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    google.maps.event.addListener(map, 'click', function () {
+      getInfo();
+    });
+
+    function getInfo() {
+      let city = [
+        {
+          lat: 23.43344,
+          lng: 32.43242
+        }
+      ];
+
+      let infowindow = new google.maps.InfoWindow();
+      let service = new google.maps.places.PlacesService(map);
+
+      service.getDetails({
+        placeId: 'ChIJN1t_tDeuEmsRUsoyG83frY4'
+      }, function(place, status) {
+        if (status === google.maps.places.PlacesServiceStatus.OK) {
+          let marker = new google.maps.Marker({
+            map: map,
+            position: place.geometry.location
+          });
+          google.maps.event.addListener(marker, 'click', function() {
+            infowindow.setContent('<div><strong>' + place.name + '</strong><br>' +
+              'Place ID: ' + place.place_id + '<br>' +
+              place.formatted_address + '</div>');
+            infowindow.open(map, this);
+          });
+        }
+      });
+    }
 
     const locations = [
 
@@ -207,7 +355,7 @@ export class ExploreComponent implements OnInit {
   }
 
   getInfo() {
-
+    console.log("get info works");
   }
 
   setMarkers(locations) {
@@ -257,3 +405,4 @@ export class ExploreComponent implements OnInit {
   }
 
 }
+
