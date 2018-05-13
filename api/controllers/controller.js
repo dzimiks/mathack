66417430b3d7a63a8b6d7f959f754459d2b8840e
unknown <brankofulurija@gmail.com>
2018-05-13 04:30:20 +0200

    Calculating top spots

--- api/controllers/controller.js
+++ api/controllers/controller.js
@@ -1,35 +1,119 @@
-// <<<<<<< Updated upstream
-// var exports = module.exports = {};
-
-// exports.explore = function (req, res) {
-//   console.log(req.body.city);
-//   res.status(200).json({
-//     "path": [
-//       {lat: 44.817186, lng: 20.560856},
-//       {lat: 44.835186, lng: 20.530856},
-//       {lat: 44.834186, lng: 20.460856},
-//       {lat: 44.817186, lng: 20.560856},
-//       {lat: 44.812186, lng: 20.560856},
-//       {lat: 44.814186, lng: 20.560856},
-//       {lat: 44.817186, lng: 20.560846},
-//       {lat: 44.817186, lng: 20.530856},
-//       {lat: 44.811184, lng: 20.560856},
-//       {lat: 44.812183, lng: 20.530856},
-//     ]
-//   });
-// =======
 var exports = module.exports = {}
 var googleMaps = require('./googleMaps');
+var unique = require('array-unique');
 
 exports.explore = function (req, res) {
-    console.log(req.body.city);
-    var places = googleMaps.findPlaces({ lat: 43.78344990000001, lng: 19.2934517 }, "school", function (places) {
-        console.log(places);
-        res.status(200).json({
-            "path": places
+    let sportTypes = ['stadium', 'park'];
+    let nightLifeTypes = ['bar', 'night_club', 'restaurant', 'cafe'];
+    let historicalTypes = ['art_gallery', 'church', 'museum', 'synagogue', 'mosque', 'fortress'];
+    let natureTypes = ['campground', 'zoo', 'park', 'river'];
+
+    let sportPlaces = [];
+    let nightlifePlaces = [];
+    let historicalPlaces = [];
+    let naturePlaces = [];
+
+    let result = [];
+    let cnt = 0;
+    for (let i = 0; i < sportTypes.length; i++) {
+        googleMaps.findPlaces(req.body.city, sportTypes[i], function (err, places) {
+            if (err) {
+                console.log(err);
+            } else {
+                sportPlaces = sportPlaces.concat(places);
+            }
+            cnt++;
+            if (cnt === sportTypes.length) {
+                sportPlaces = unique(sportPlaces);
+                cnt = 0;
+                for (let i = 0; i < nightLifeTypes.length; i++) {
+                    googleMaps.findPlaces(req.body.city, nightLifeTypes[i], function (err, places) {
+                        if (err) {
+                            console.log(err);
+                        } else {
+                            nightlifePlaces = nightlifePlaces.concat(places);
+                        }
+                        cnt++;
+                        if (cnt === nightLifeTypes.length) {
+                            nightlifePlaces = unique(nightlifePlaces);
+                            cnt = 0;
+                            for (let i = 0; i < historicalTypes.length; i++) {
+                                googleMaps.findPlaces(req.body.city, historicalTypes[i], function (err, places) {
+                                    if (err) {
+                                        console.log(err);
+                                    } else {
+                                        historicalPlaces = historicalPlaces.concat(places);
+                                    }
+                                    cnt++;
+                                    if (cnt === historicalTypes.length) {
+                                        historicalPlaces = unique(historicalPlaces);
+                                        cnt = 0;
+                                        for (let i = 0; i < natureTypes.length; i++) {
+                                            googleMaps.findPlaces(req.body.city, natureTypes[i], function (err, places) {
+                                                if (err) {
+                                                    console.log(err);
+                                                } else {
+                                                    naturePlaces = naturePlaces.concat(places);
+                                                }
+                                                cnt++;
+                                                if (cnt === natureTypes.length) {
+                                                    naturePlaces = unique(naturePlaces);
+
+                                                    let finalResult = exports.predict(req, sportPlaces, historicalPlaces, naturePlaces, nightlifePlaces);
+                                                    res.status(200).json({
+                                                        "path": finalResult
+                                                    });
+                                                }
+                                            });
+                                        }
+                                    }
+                                });
+                            }
+                        }
+                    });
+                }
+            }
         });
-    });
+    }
+
 };
 
-
-
+exports.predict = function(req, sportPlaces, historicPlaces, naturePlaces, nightlifePlaces) {
+    let centroid = {
+        sport: parseFloat(req.body.sportWeight),
+        historical: parseFloat(req.body.historicalWeight),
+        nightlife: parseFloat(req.body.nightlifeWeight),
+        nature: parseFloat(req.body.natureWeight)
+    };
+    for (let i = 0; i < nightlifePlaces.length; i++) {
+        nightlifePlaces[i].rating *= centroid.nightlife;
+        if (nightlifePlaces[i] === NaN) {
+            nightlifePlaces[i] = 0;
+        }
+    }
+    for (let i = 0; i < sportPlaces.length; i++) {
+        sportPlaces[i].rating *= centroid.sport;
+        if (sportPlaces[i] === NaN) {
+            sportPlaces[i] = 0;
+        }
+    }
+    for (let i = 0; i < historicPlaces.length; i++) {
+        historicPlaces[i].rating *= centroid.historical;
+        if (historicPlaces[i] === NaN) {
+            historicPlaces[i] = 0;
+        }
+    }
+    for (let i = 0; i < naturePlaces.length; i++) {
+        naturePlaces[i].rating *= centroid.nature;
+        if (naturePlaces[i] === NaN) {
+            naturePlaces[i] = 0;
+        }
+    }
+    let result = nightlifePlaces.concat(sportPlaces.concat(historicPlaces.concat(naturePlaces)));
+    result.sort(function(a, b){return b.rating-a.rating});
+    result = result.splice(0, 10);
+    for (let i = 0; i < result.length; i++) {
+        result[i] = result[i].location;
+    }
+    return result;
+}
--- api/controllers/googleMaps.js
+++ api/controllers/googleMaps.js
@@ -1,26 +1,33 @@
 var exports = module.exports = {}
 
-var googleMapsClient = require('@google/maps');
+var googleMapsClient = require('@google/maps').createClient({
+    key: 'AIzaSyCKF9usR1uRoBmIYxvQUO7DBt01rFyXH9A'
+});
 
-exports.findPlaces = function (location, type, fn) {
-    googleMapsClient = googleMapsClient.createClient({
-        key: 'AIzaSyCKF9usR1uRoBmIYxvQUO7DBt01rFyXH9A'
-    });
-    googleMapsClient.placesNearby({
-        location: location,
-        radius: 5000,
-        type: type
-    }, function (err, response) {
+exports.findPlaces = function (city, type, fn) {
+    googleMapsClient.geocode({
+        address: city
+    }, function (err, res) {
         if (!err) {
-            let result = response.json.results;
-            var places = [];
-            result.forEach(element => {
-                places.push(element.geometry.location);
+            googleMapsClient.placesNearby({
+                location: res.json.results[0].geometry.location,
+                radius: 4000,
+                type: type
+            }, function (err, response) {
+                if (!err) {
+                    let result = response.json.results;
+                    var places = [];
+                    for (let i = 0; i < result.length; i++) {
+                        let element = result[i];
+                        places.push({location: element.geometry.location, types: element.types, rating: element.rating});
+                    }
+                    fn(null, places);
+                } else {
+                    fn(err, null);
+                }
             });
-            fn(places);
         } else {
-            console.log(err);
-            return [];
+            fn(err, null);
         }
     });
 };
